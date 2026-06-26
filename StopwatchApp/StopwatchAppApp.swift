563cc3eacddf9f78fbd41bea888f34c3c37376d8
marco.caruso <49186481+MrCaruso201@users.noreply.github.com>
2026-06-26 11:55:19 +0200

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Marco Caruso on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
