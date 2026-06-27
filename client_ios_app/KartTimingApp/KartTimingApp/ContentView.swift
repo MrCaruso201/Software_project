c0ce6232a920c3740105e7e130385c5ed1ce522c
marco.caruso <49186481+MrCaruso201@users.noreply.github.com>
2026-06-27 14:37:27 +0200

    nuovi colori

--- client_ios_app/KartTimingApp/KartTimingApp/ContentView.swift
+++ client_ios_app/KartTimingApp/KartTimingApp/ContentView.swift
@@ -178,7 +178,7 @@ class KartTimingManager: ObservableObject {
 extension Color {
     static let kartBG      = Color(red: 0.05, green: 0.05, blue: 0.07)
     static let kartPanel   = Color(red: 0.10, green: 0.10, blue: 0.13)
-    static let kartAccent  = Color(red: 0.96, green: 0.77, blue: 0.09)   // giallo race
+    static let kartAccent  = Color(red: 0.96, green: 0.14, blue: 0.21)   // rosso
     static let kartGreen   = Color(red: 0.13, green: 0.76, blue: 0.37)
     static let kartRed     = Color(red: 0.93, green: 0.27, blue: 0.27)
     static let kartDim     = Color.white.opacity(0.35)
